8f7798fe41e774ac889edb50d2ed4fdab52b4775
Anthony <gamerseth@protonmail.com>
2019-12-15 11:20:01 -0500

    hi

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from . import db, login_manager
 from flask import Blueprint, render_template, flash, redirect, url_for, request, current_app
 from flask_login import login_required, current_user
 from .models import *
-main = Blueprint('main', __name__)
+import ast
 
+main = Blueprint('main', __name__)
+L = "google.com, anthony, password|facebook.com, bob, qoojadkm|"
 @main.route('/vault')
 def handle_needs_login():
+    global L
+    print(L, flush=True)
     print(User.Logins)
     #current_user.Logins += "google.com, anthony, password"
-    L = "google.com, anthony, password|facebook.com, bob, qoojadkm|"
+
     logins = L.split("|")
     l = []
     for i in range(0, len(logins)):
@@ -26,3 +30,22 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.usr)
+@main.route('/new', methods=['POST'])
+def new():
+    global L
+    saveName = request.form.get('saveName')
+    savePassword = request.form.get('savePassword')
+    saveWebsite = request.form.get('saveWebsite')
+
+
+    L +=  saveWebsite + ", " + savePassword + ", " + saveName + "|"
+    return handle_needs_login()
+
+@main.route('/delete', methods=['POST'])
+def delete():
+    global L
+    toDelete = request.form.get('delete')
+    toDelete = ast.literal_eval(toDelete)
+    L = L.strip(toDelete[0] + ", " + toDelete[1] + ", " + toDelete[2] + "|")
+    print(L, flush=True)
+    return handle_needs_login()
